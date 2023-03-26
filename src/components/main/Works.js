4ad4401386753a146fc980c2b14920475ef62bc3
BerkaouiMohamed <108631143+BerkaouiMohamed@users.noreply.github.com>
2023-03-26 23:21:25 +0100

    Create Works.js

---
+++ src/components/main/Works.js
@@ -0,0 +1,14 @@
+import './UnderConstruction.css';
+
+const Works = () => {
+  return (
+    <div className="container">
+      <div className="wrapper">
+        <h1>This page is under construction</h1>
+        <p>Please check back soon!</p>
+      </div>
+    </div>
+  );
+};
+
+export default Works;
