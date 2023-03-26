460717c2fb1e4737b1be30a99e48975c9877392c
BerkaouiMohamed <108631143+BerkaouiMohamed@users.noreply.github.com>
2023-03-26 23:37:05 +0100

    Update Works.js

--- src/components/main/Works.js
+++ src/components/main/Works.js
@@ -1,12 +1,49 @@
 import './UnderConstruction.css';
+import { motion } from 'framer-motion';
 
 const Works = () => {
+ const containerVariants = {
+    animate: {
+      transition: {
+        duration: 2,
+        ease: 'easeInOut',
+        repeat: Infinity,
+        repeatType: 'mirror',
+      },
+    },
+  };
+
+  const messageVariants = {
+    initial: {
+      opacity: 0,
+      y: -20,
+    },
+    animate: {
+      opacity: 1,
+      y: 0,
+      transition: {
+        duration: 0.5,
+        ease: 'easeInOut',
+        repeat: Infinity,
+        repeatType: 'mirror',
+      },
+    },
+  };
+
   return (
     <div className="containerwork">
-      <div className="wrapperwork">
-        <h1>This page is under construction</h1>
-        <p>Please check back soon!</p>
-      </div>
+      <motion.div
+        className="wrapperwork"
+        variants={containerVariants}
+        animate="animate"
+      >
+        <motion.h1 variants={messageVariants} initial="initial" animate="animate">
+          This page is under construction
+        </motion.h1>
+        <motion.p variants={messageVariants} initial="initial" animate="animate">
+          Please check back soon!
+        </motion.p>
+      </motion.div>
     </div>
   );
 };
